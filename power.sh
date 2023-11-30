db37e91a05de44f5230ba71d635534234c143063
hmidani abdelilah <39446276+hmidani-abdelilah@users.noreply.github.com>
2023-11-30 12:32:53 +0100

    Update power.sh

--- power.sh
+++ power.sh
@@ -2,7 +2,8 @@
 capacity=/sys/class/power_supply/BAT1/capacity
 STATUSS=/sys/class/power_supply/BAT1/status
 STATUSS="${STATUSS//[$'\t\r\n ']}"
+username=abdelilah
 if [ $(cat ${capacity}) -lt 30 ] && [ $(cat ${STATUSS}) == Discharging ]; then
-	su abdelilah -c "DISPLAY=':0' notify-send -t 60000  'The Power is Low Plug The Power Alimentation Now' --icon=csd-power --app-name='csd power'"
+	su ${username} -c "DISPLAY=':0' notify-send -t 60000  'The Power is Low Plug The Power Alimentation Now' --icon=csd-power --app-name='csd power'"
 fi
 
